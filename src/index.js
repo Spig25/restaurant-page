0077c26a5710d2c02c329b71f47534bee4b2880a
Kasey <kasey.murdoch@yahoo.com>
2022-03-29 10:39:37 -0700

    Used webpack to make page via JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log(`Testing testing`)
+console.log(`Testing testing`);
+
+const content = document.querySelector(`#content`);
+const header = document.createElement(`h1`);
+header.textContent = `Welcome!`;
+content.appendChild(header);
+const image = document.createElement(`img`);
+image.src = `../src/test.png`
+content.appendChild(image);
+const para = document.createElement(`p`);
+para.textContent = `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur exercitationem, libero nam nostrum recusandae eum quaerat consequuntur facere ea hic numquam voluptatem porro temporibus qui obcaecati? Accusamus voluptatum minima minus`;
+content.appendChild(para);
