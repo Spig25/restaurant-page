722f83c49017c049621259f82a148624903958da
Kasey <kasey.murdoch@yahoo.com>
2022-03-29 10:39:37 -0700

    Used webpack to make page via JS

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-console.log("Testing testing");
+(()=>{console.log("Testing testing");const e=document.querySelector("#content"),t=document.createElement("h1");t.textContent="Welcome!",e.appendChild(t);const n=document.createElement("img");n.src="../src/test.png",e.appendChild(n);const o=document.createElement("p");o.textContent="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur exercitationem, libero nam nostrum recusandae eum quaerat consequuntur facere ea hic numquam voluptatem porro temporibus qui obcaecati? Accusamus voluptatum minima minus",e.appendChild(o)})();
