e5f17ef2d8016e19297cfcca3be561d1d5ca8117
Kasey <kasey.murdoch@yahoo.com>
2022-03-30 12:28:16 -0700

    Can toggle between displaying separate js files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
-import initialPage from "./initial";
+import homePage from "./initial";
+import menu from "./menu";
+import contact from "./contact";
 import "./style.css";
 
+homePage()
 
-initialPage()
+const homeButton = document.querySelector(`.home`)
+const menuButton = document.querySelector(`.menu`)
+const contactButton = document.querySelector(`.contact`)
+
+homeButton.addEventListener(`click`, () => {
+    homePage()
+})
+
+menuButton.addEventListener(`click`, () => {
+    menu()
+})
+
+contactButton.addEventListener(`click`, () => {
+    contact()
+})
